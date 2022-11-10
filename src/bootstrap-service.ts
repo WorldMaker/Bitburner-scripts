fcc75ef8d02b18eb32f535c106c25f99d8694535
Max Battcher <me@worldmaker.net>
2022-11-09 19:15:21 -0500

    🐛 Fix missing dependency

--- src/bootstrap-service.ts
+++ src/bootstrap-service.ts
@@ -61,7 +61,7 @@ export async function main(ns: NS) {
 	const payloadService = new PayloadService()
 	const servers = new ServerCacheService(ns)
 	const purchaseService = new PurchaseService(ns, servers, hacknetNodes)
-	const toyPurchaseService = new ToyPurchaseService(ns, servers)
+	const toyPurchaseService = new ToyPurchaseService(ns, logger, servers)
 
 	let lastServersCount = 0
 	let lastRootedCount = 0
