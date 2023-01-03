64ee1d2eb996185b11eb1c84a7d17ff2bd584970
Max Battcher <me@worldmaker.net>
2023-01-02 23:15:54 -0500

    🐱‍💻 Memoize total ways to sum 1 solver

--- src/cct/total-ways-to-sum1.ts
+++ src/cct/total-ways-to-sum1.ts
@@ -13,20 +13,60 @@ It is possible write four as a sum in exactly four different ways:
 How many different distinct ways can the number 17 be written as a sum of at least two positive integers?
 */
 
+import { AsyncIterableX } from '@reactivex/ix-esnext-esm/asynciterable/asynciterablex'
+import { memoize } from '@reactivex/ix-esnext-esm/asynciterable/operators/memoize'
 import { Cooperative } from '.'
+
+const { from } = AsyncIterableX
+
+class MemoMap {
+	private map = new Map<number, Map<number, AsyncIterable<number[]>>>()
+
+	get(n: number, i: number) {
+		if (this.map.has(n)) {
+			return this.map.get(n)!.get(i)
+		}
+		return undefined
+	}
+
+	set(n: number, i: number, value: AsyncIterable<number[]>) {
+		let nmap = this.map.get(n)
+		if (!nmap) {
+			nmap = new Map<number, AsyncIterable<number[]>>()
+			this.map.set(n, nmap)
+		}
+		nmap.set(i, value)
+	}
+}
 
 async function* partition(
 	n: number,
 	cooperative: Cooperative,
-	I = 1
+	I = 1,
+	memo = new MemoMap()
 ): AsyncIterable<number[]> {
 	yield [n]
 	for (let i = I; i < Math.floor(n / 2) + 1; i++) {
-		for await (const p of partition(n - i, cooperative, i)) {
+		for await (const p of memoizePartition(n - i, cooperative, i, memo)) {
 			yield [i, ...p]
 		}
 	}
 	await cooperative(() => `partioning ${n}; ${I}`)
+}
+
+function memoizePartition(
+	n: number,
+	cooperative: Cooperative,
+	I = 1,
+	memo = new MemoMap()
+): AsyncIterable<number[]> {
+	const existing = memo.get(n, I)
+	if (existing) {
+		return existing
+	}
+	const value = from(partition(n, cooperative, I, memo)).pipe(memoize())
+	memo.set(n, I, value)
+	return value
 }
 
 export async function sumPartitions(input: number, cooperative: Cooperative) {
