6a9d24780a26ff4956e7bbe89b84a1cb8c577ccf
Max Battcher <me@worldmaker.net>
2023-02-05 13:55:07 -0500

    🐱‍💻 Replace hardcoded purchased server Max RAM

--- src/services/toy-purchase/server-upgrader.ts
+++ src/services/toy-purchase/server-upgrader.ts
@@ -7,13 +7,15 @@ import { ServerCacheService } from '../server-cache'
 
 const { from } = IterableX
 
-const MaxRam = 2 ** 20
-
 export class ServerUpgrader implements ToyPurchaser {
+	readonly maxRam: number
+
 	constructor(
 		private ns: NS,
 		private servers: ServerCacheService<ServerTarget>
-	) {}
+	) {
+		this.maxRam = this.ns.getPurchasedServerMaxRam()
+	}
 
 	purchase(budget: number): number {
 		// *** Attempt to double purchased server RAM ***
@@ -24,10 +26,10 @@ export class ServerUpgrader implements ToyPurchaser {
 		)
 
 		for (const server of purchasedServersByRam) {
-			if (server.getMaxRam() >= MaxRam) {
+			if (server.getMaxRam() >= this.maxRam) {
 				break
 			}
-			const doubleRam = Math.min(MaxRam, server.getMaxRam() * 2)
+			const doubleRam = Math.min(this.maxRam, server.getMaxRam() * 2)
 			const cost = this.ns.getPurchasedServerUpgradeCost(server.name, doubleRam)
 			if (budget > cost) {
 				if (this.ns.upgradePurchasedServer(server.name, doubleRam)) {
